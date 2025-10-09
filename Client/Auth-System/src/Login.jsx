919acdd3452498c0299ceb1523d9068c1aa2822e
Krishna-zz <krishnagunjal022@gmail.com>
2025-10-09 09:31:24 +0530

    Login and register Complete

--- Client/Auth-System/src/Login.jsx
+++ Client/Auth-System/src/Login.jsx
@@ -1,9 +1,24 @@
-
+import { useState } from "react"
+import {loginUser} from './Services/Api'
 
 function Login(){
+      
+    const [formdata, setFormdata] = useState({email:"", password:""})
+    
+    const handleChange = (e) => {
+        setFormdata({...formdata, [e.target.name]: e.target.value})
+    }
 
-    
-    
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+        try {
+            const {data} = await loginUser(formdata)
+            localStorage.setItem('token', data.token)
+            alert('Login Successful')
+        } catch (error) {
+            alert(error.response?.data?.message || 'Login failed');
+        }
+    }
 
     return(
         <div className="bg-gray-400 min-h-screen flex justify-center items-center ">
@@ -12,14 +27,14 @@ function Login(){
                     <h1 className="font-bold text-2xl mb-5 flex justify-center">Login</h1>
                 
                 
-                <form  action=""  className="flex flex-col">
+                <form  onSubmit={handleSubmit} action=""  className="flex flex-col">
                     <label  htmlFor="">Email:</label>
-                    <input type="text" className="px-4 py-2 rounded-2xl shadow-2xl ml-2 mb-6" />
+                    <input type="text" name="email" placeholder="Email" onChange={handleChange} className="px-4 py-2 rounded-2xl shadow-2xl ml-2 mb-6" />
                     <br />
                     <label htmlFor="">Password:</label>
-                    <input type="text" className="px-4 py-2 rounded-2xl shadow-2xl ml-1 mb-6" />
+                    <input type="text" name="password" placeholder="Password" onChange={handleChange} className="px-4 py-2 rounded-2xl shadow-2xl ml-1 mb-6" />
 
-                    <button className="bg-green-500 py-2 px-4 rounded-2xl shadow-2xl">
+                    <button type="submit" className="bg-green-500 py-2 px-4 rounded-2xl shadow-2xl">
                         Login
                     </button>
 
--- Server/Routes/auth.routes.js
+++ Server/Routes/auth.routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../Models/User.model')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 
 router.post('/register', async(req, res) => {
@@ -32,15 +33,21 @@ router.post('/register', async(req, res) => {
 router.post('/login', async(req, res) => {
     
     try {
-        const {email, password} = req.body
+       const {email, password} = req.body
 
-        const user = await User.findOne({email}) 
-        if(!user) return res.status(400).json({message:"User not found"})
+       const user = await User.findOne({email});
+       if(!user) return res.status(400).json({message:"User not found"})
 
-        const isMatch = await bcrypt.compare(password, user.password)
-        if(!isMatch) return res.status(400).json({message:"Invalid credentials"})
+       const IsMatch = await bcrypt.compare(password, user.password)
+       if(!IsMatch) return res.status(400).json({message:"Invalid Credentials"})
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET , {expiresIn:'1h'}) 
+        const {password: mwt , ...userData} = user._doc;
+
+        res.json({message:"Login Successful", token, user: userData})
+
     } catch (error) {
-        
+        res.status(500).json({message:"Internal Server Error"})
     }
 })
 
